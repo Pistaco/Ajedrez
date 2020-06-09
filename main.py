ad024350cf3656274cea78f588b6258f0177c703
IgnacioTorre <helopython123@gmail.com>
2020-06-09 08:19:05 -0400

    Funcion de ayuda, refactorizado _info_

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from piezas import Pieza, Peon
 from tablero import Tablero
 
+def opciones_(evaluar, opciones, mensaje, operador = "negacion"):
+    if operador == "negacion":
+        if evaluar not in opciones:
+            print(mensaje)
+            return False
+        return True
+    if operador == "==":
+        if evaluar == opciones:
+            return True
+        else:
+            print(mensaje)
+            return False
+
 
 class Ajedres:
     def __init__(self):
@@ -17,26 +30,22 @@ class Ajedres:
         while True:
             input_ = input(pregunta)
             keys = tuple("ABCDEFGH")
-            if len(input_) == 3:
+            if opciones_(len(input_), 3, "Ingresaste un valor demasiado largo", "=="):
                 a, b, c = input_
                 a, b, c = a.upper() ,b.upper(), int(c)
-                if a not in "KQRBNP":
-                    print("no ingresaste una pieza valida")
-                elif b not in keys:
-                    print("ingresaste un valor incorrecto")
-                elif c not in tuple(range(1, 8)):
-                    print("ingresaste un valor incorrecto")
-                else:
-                    tupla = (a, keys.index(b), c - 1)
-                    return tupla
-            else:
-                print("Valor demasiado largo")
+                if opciones_(a, "KQRBNP", "no ingresaste una pieza valida"):
+                    if opciones_(b, keys, "ingresaste una row incorrecta"):
+                        if opciones_(c, tuple(range(1,8)), "ingresaste una columna incorrecta"):
+                            tupla = (a, keys.index(b), c - 1)
+                            return tupla
 
     def turno(self):
         while True:
             coo = self._info_("Que pieza desea mover?\n")
             plz = self._info_("Hacia donde quiere moverla?\n")
-            if self._Seleccionador_(coo):
+            if self.tablero.GET(coo, bol=True) is False:
+                print("No existe pieza donde seleccionaste")
+            elif self._Seleccionador_(coo):
                 if self.pactual.mover(coo, plz):
                     break
 
